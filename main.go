f75023ff0b2e3bb29dfa6c4fa275488df23d169d
Heru Susanto <heruxi@gmail.com>
2021-11-13 10:31:28 +0700

    update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -12,6 +13,32 @@ func index(res http.ResponseWriter, req *http.Request) {
 func upload(res http.ResponseWriter, req *http.Request) {
 	req.ParseMultipartForm(10 << 20)
 
+	file, handler, err := req.FormFile("file")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("file uploaded: ", handler.Filename)
+	fmt.Println("size: ", handler.Size)
+	fmt.Println("header: ", handler.Header)
+
+	tempFile, err := ioutil.TempFile("tempfiles", handler.Filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(res, "Successfully Uploaded File\n")
 }
 
 func main() {
